d90d2dda30b871f9dd8c6c5861c3b8beca1d41fb
Weuller <weullermarcos@hotmail.com>
2021-05-11 11:11:15 -0300

    projeto filmes em cartaz - 2

--- filmesCartaz/App.js
+++ filmesCartaz/App.js
@@ -4,7 +4,7 @@
  */
 
 import React, {useState, useEffect} from 'react';
-import {SafeAreaView, Text, Button, FlatList, Image, View} from 'react-native';
+import {SafeAreaView, Text, Button, FlatList, Image, View, StyleSheet, ActivityIndicator} from 'react-native';
 
 
 const App = () => {
@@ -12,40 +12,116 @@ const App = () => {
   // const para armazenar os filmes
   const [movies, setMovies] = useState([]);
 
-  const handleLoadButton = async () => {
-  
-    // Faz uma requisicao sincrona
-    const req = await fetch("https://api.b7web.com.br/cinema/");
+  // const para o botao de loading
+  const [loading, setLoading] = useState(false);
+
+  // vai rodar uma unica vez ao iniciar o aplicativo
+  useEffect(()=>{
+
+    const requestMovies = async () => {
+
+      // ativa o carregando
+      setLoading(true);
+
+      // Faz uma requisicao sincrona
+      const req = await fetch("https://api.b7web.com.br/cinema/");
     
-    // deserializa o json recebido
-    const json = await req.json();
+      // deserializa o json recebido
+      const json = await req.json();
 
-    if(json){
+      if(json){
 
-      setMovies(json);
-    }
+        // alert(json.length);
+        setMovies(json);
+      }
 
-    // alert(json.length);
+      // desativa o carregando
+      setLoading(false);
+    } 
 
-  }
+    // chama funcao para requisitar os filmes
+    requestMovies();
+  }, []);
 
   return(
-    <SafeAreaView> 
-      <Button title='Carregar' onPress={handleLoadButton}/>
-      <Text> Total de filmes: {movies.length} </Text>
-      <FlatList data={movies}
-                renderItem = {({item}) => (
+    <SafeAreaView style={styles.container}> 
 
-                  <View> 
-                    <Image source={{uri: item.avatar}} style={{width: 200, height: 200}}/>
-                    <Text> {item.titulo} </Text>
-                  </View>
-                )}
-                keyExtractor = {item => item.titulo}
+      {loading &&
+        <View style = {styles.loadingArea}> 
+          <ActivityIndicator size='large' color='#FFFFFF'/>
+          <Text style = {styles.loadingText}> Carregando... </Text>
+        </View>
+      }
 
-      />
+      {!loading &&  
+        <> 
+          <Text style={styles.totalMovieText}> Total de filmes: {movies.length} </Text>
+          <FlatList data={movies}
+                    style={styles.list}
+                    renderItem = {({item}) => (
+
+                      <View style={styles.movieItem}> 
+                        <Image source={require('./src/images/filme.jpg')} 
+                              style={styles.movieImage}
+                              resizeMode='contain'/>
+
+                        <Text style={styles.movieTitle}> {item.titulo} </Text>
+                      </View>
+                    )}
+                    keyExtractor = {item => item.titulo}
+          />
+        </>
+
+      }
+
     </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+
+  container:{
+    flex: 1,
+    backgroundColor: '#333333'
+  },
+
+  totalMovieText: {
+    color: '#FFFFFF',
+    fontSize: 18,
+    textAlign: 'center',
+    marginTop: 10,
+    marginBottom: 10
+  },
+
+  list: {
+    flex: 1
+  },
+
+  movieItem: {
+    marginBottom: 30
+  },
+
+  movieImage: {
+    height: 400
+  },
+
+  movieTitle: {
+    color: '#FFFFFF',
+    fontSize: 24,
+    textAlign: 'center',
+    marginTop: 5,
+  },
+
+  loadingArea: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  loadingText: {
+    color: '#FFFFFF'
+  }
+
+});
 
 export default App;
