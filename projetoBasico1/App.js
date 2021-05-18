85bcd020f9440f79cfb0127ba5b57242ce749ef7
Weuller <weullermarcos@hotmail.com>
2021-05-18 20:35:42 -0300

    usando props

--- projetoBasico1/App.js
+++ projetoBasico1/App.js
@@ -5,17 +5,28 @@
 
 
  import React, {Component} from 'react';
- import {View, Text} from 'react-native';
+ import {View, Text, Image} from 'react-native';
 
  export default class ProjetoBasico extends Component{
 
   render(){
+
+    // criando uma variavel
+    let imagem = {uri: 'https://www.google.com.br/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png'};
+
     return(
 
       <View> 
         <Text> Ola Mundo </Text>
         <Text> Ola Mundo </Text>
-        <Text> Ola Mundo </Text>
+        <Text style={{ fontSize: 25, 
+                       color:'red',
+                       margin: 20}}> 
+            Ola Mundo 
+        </Text>
+
+        <Image source={imagem}
+               style={{height: 300, width: 300}}/>
       </View>
 
     );
