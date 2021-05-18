9d52c2a5116b4698c295b382c7e3631d69628376
Weuller <weullermarcos@hotmail.com>
2021-05-18 20:12:25 -0300

    projeto para react native basico

---
+++ projetoBasico1/App.js
@@ -0,0 +1,23 @@
+/**
+ * React Native Basico
+ * Weuller - 18/05/2021
+ */
+
+
+ import React, {Component} from 'react';
+ import {View, Text} from 'react-native';
+
+ export default class ProjetoBasico extends Component{
+
+  render(){
+    return(
+
+      <View> 
+        <Text> Ola Mundo </Text>
+        <Text> Ola Mundo </Text>
+        <Text> Ola Mundo </Text>
+      </View>
+
+    );
+  }
+ }
